7841b08664332e64514cbe9b9fcce1d4a44a5bcd
Knuth Konrad <Knuth.Konrad@sta-travel.de>
2021-03-06 15:49:19 +0100

    Add project files.

---
+++ libBAUtilCS/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libBAUtilCS
+{
+    public class Class1
+    {
+    }
+}
